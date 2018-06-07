2dc45443b1c5b3acd5e168aae6914c2229c79bcc
Qinxiao Yu <178963605@qq.com>
2018-06-07 16:45:37 +0800

    DestroyAndRepair

--- rich VRP/Neighborhoods/DestroyRepair/DestroyAndRepair.cs
+++ rich VRP/Neighborhoods/DestroyRepair/DestroyAndRepair.cs
@@ -11,18 +11,20 @@ namespace rich_VRP.Neighborhoods.DestroyRepair
     {
         
 
-        public DestroyAndRepair(Solution _solution)
-        {
-            solution = _solution;          
-            solution.UnVisitedCus = new List<Customer>();
+        public DestroyAndRepair()
+        {         
         }
         /// <summary>
         /// 如果一条路线上商户的数量小于threshold_node个，则删除此线路
         /// </summary>
         /// <param name="threshold_node"></param>
-        public void DestroyShortRoute(int threshold_node)
+        public Solution DestroyShortRoute(Solution solution, int threshold_node)
         {
             Solution new_sol = solution.Copy();
+            if (solution.UnVisitedCus==null)
+            {
+                solution.UnVisitedCus = new List<Customer>();
+            }
             for (int i = new_sol.Routes.Count - 1; i > 0 ;i--)
             {
                 Route r = new_sol.Routes[i];
@@ -45,12 +47,17 @@ namespace rich_VRP.Neighborhoods.DestroyRepair
                     }
 
                 }
-            }        
+            }
+            return solution;        
         }
 
-        public void DestroyWasteRoute(double percent)
+        public Solution DestroyWasteRoute(Solution solution, double percent)
         {
             Solution new_sol = solution.Copy();
+            if (solution.UnVisitedCus == null)
+            {
+                solution.UnVisitedCus = new List<Customer>();
+            }
             for (int i = new_sol.Routes.Count - 1; i > 0; i--)
             {
                 Route r = new_sol.Routes[i];
@@ -77,18 +84,23 @@ namespace rich_VRP.Neighborhoods.DestroyRepair
 
                 }
             }
+            return solution;
         }
 
-        public void DestroyAfternoonNodes(double cuttingpoint)
+        public void DestroyAfternoonNodes(Solution solution, double cuttingpoint)
         {
             Solution new_sol = solution.Copy();
+            if (solution.UnVisitedCus == null)
+            {
+                solution.UnVisitedCus = new List<Customer>();
+            }
             for (int i = new_sol.Routes.Count - 1; i > 0; i--)
             {
                 Route r = new_sol.Routes[i];
                 double totalWeight = r.GetTotalWeight();
                 double totalVolume = r.GetTotalVolume();
-                if (totalVolume < percent * Problem.VehTypes[r.AssignedVeh.TypeId - 1].Volume
-                    || totalWeight < percent * Problem.VehTypes[r.AssignedVeh.TypeId - 1].Weight)
+                if (totalVolume < cuttingpoint * Problem.VehTypes[r.AssignedVeh.TypeId - 1].Volume
+                    || totalWeight < cuttingpoint * Problem.VehTypes[r.AssignedVeh.TypeId - 1].Weight)
                 {
                     solution.Routes.RemoveAt(i);
                     int veh_id = r.AssignedVeh.VehId;
